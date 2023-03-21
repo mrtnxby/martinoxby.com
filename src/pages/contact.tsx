121b88d5a35d0838b688969e0237e2bd4d504bd7
Martin Oxby <martin.oxby26@gmail.com>
2023-03-21 02:07:21 +0000

    feat(website): create first draft

---
+++ src/pages/contact.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <Layout>
+      <Head>
+        <title>Contact | oxby.io</title>
+      </Head>
+      <h1>Contact me</h1>
+    </Layout>
+  );
+}
