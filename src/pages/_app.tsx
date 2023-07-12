5ffc0fede1e06767ec35962e37a933404c1e0bfd
Martin Oxby <martinoxby@zoa.io>
2023-07-12 20:38:40 +0100

    Change colours and fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto_Mono } from "next/font/google";
+// If loading a variable font, you don't need to specify the font weight
+const roboto = Roboto_Mono({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
