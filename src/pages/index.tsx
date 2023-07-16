16c1e5df1ce5579f89ffd0bd1d54074ac1376921
Martin Oxby <martinoxby@zoa.io>
2023-07-16 09:06:51 +0100

    Please fix imports issue...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import utilStyles from "@/styles/utils.module.css";
-import Layout, { siteTitle } from "@/components/Layout";
+
 import ThemeToggle from "@/components/ThemeToggle";
 import svgStyles from "@/styles/svgStyles.module.css";
+import Layout, { siteTitle } from "@/components/Layout";
 
 export default function Home() {
   return (
